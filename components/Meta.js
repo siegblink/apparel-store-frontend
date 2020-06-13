e9bb763f5a648402e2515e92e710efe74f370032
Siegfred Balona <siegfred@callhandling.co.uk>
2020-06-13 15:07:18 +0800

    Add styled components on page, nav, and header components.

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,7 @@ export default function Meta() {
   return (
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <meta charset='utf-8' />
+      <meta charSet='utf-8' />
       <link rel='shortcut icon' href='/static/favicon.icon.png' />
       <link rel='stylesheet' href='/static/nprogress.css' />
       <title>Sick Fits!</title>
